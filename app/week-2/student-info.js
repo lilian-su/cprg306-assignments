976cd5b0e01c5cd37c8d0a7213d0685ab836e0dc
lilian-su <liliank.497@gmail.com>
2024-05-20 20:04:22 -0600

    week 2 assignment
    week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Lilian Su</h1>
+      <Link href="https:github.com/lilian-su">https://github.com</Link>
+    </main>
+  );
+}
