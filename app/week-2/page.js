6552ebac77fe480db29b84fe096c2c532074f178
lilian-su <liliank.497@gmail.com>
2024-05-28 12:39:52 -0600

    updated styling page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main>
+    <main className="h-screen p-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-red-300">
         <h1>Shopping List</h1>
         <StudentInfo />
 
