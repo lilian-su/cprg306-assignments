fc6d54afb8c331620108bfa57f3457e6762fc12e
lilian-su <liliank.497@gmail.com>
2024-05-27 22:07:33 -0600

    list shopping fixed

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
+    <main className="h-screen">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
-        <li>  
-          <Link href="week-2"> Week 2</Link>
-          
-        </li>
+        <li><Link href="week-2"> Week 2 Assignment</Link></li>
+          <li><Link href="week-3"> Week 3 Assignment</Link></li>
+          <li><Link href="week-4"> Week 4 Assignment</Link></li>
+          <li><Link href="week-5"> Week 5 Assignment</Link></li>
+        
       </ul>
     </main>
   );
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main>
+    <main className="h-screen">
         <h1>Shopping List</h1>
         <StudentInfo />
 
